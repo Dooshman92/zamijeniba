4e0a21e447dd8fd8b5c8aff53fb2bfcff49e61ec
Dooshman92 <hodzic.d92@gmail.com>
2026-01-23 21:12:02 +0100

    Updated MessagingCenterModal.tsx

--- src/components/MessagingCenterModal.tsx
+++ src/components/MessagingCenterModal.tsx
@@ -54,7 +54,7 @@ export function MessagingCenterModal({
               </svg>
             </button>
           </div>
-          <div className="flex-1 min-h-0 overflow-hidden">
+          <div className="flex-1 min-h-0 flex flex-col">
             <DirectChatModal
               conversationId={directChatConversationId}
               otherUserId={directChatOtherUserId}
